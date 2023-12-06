e8573a1bf1510070e59c1a0baa5d4a12598804d7
Chase Reidinger <chase.reidinger@adepdev.com>
2023-12-06 03:05:43 -0500

    other testing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,13 @@
 from smbus2 import SMBus
 
-# Open i2c bus 1 and read one byte from address 80, offset 0
-bus = SMBus(1)
-b = bus.read_byte_data(7, 0)
-print(b)
+# Open i2c bus 1 and read one byte from address 7 offset 0
+bus = SMBus(bus=1)
+b = bus.read_byte_data(i2c_addr=7, register=0)
+
+print(f"bus.read_byte_data: {b}")
+
+b2 = bus.read_block_data(i2c_addr=7, register=0)
+
+print(f"bus.read_block_data: {b2}")
+
 bus.close()
