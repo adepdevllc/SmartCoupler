a8894b3b275bc8c1b1dd20606e1da647a78a2fe3
Chase Reidinger <chase.reidinger@adepdev.com>
2023-12-06 03:42:41 -0500

    troubleshooting

--- src/main.py
+++ src/main.py
@@ -3,8 +3,16 @@ import board
 import busio
 from smbus2 import SMBus
 
+SENSOR_ADDRESS = "0x7"
+
+# Detect i2c devices
 i2c = busio.I2C(board.SCL, board.SDA)
 print("I2C devices found: ", [hex(i) for i in i2c.scan()])
+
+
+# Print error when sensor not found
+if not SENSOR_ADDRESS in i2c.scan():
+    print(f"Could not find fs2012 device at {SENSOR_ADDRESS}")
 
 # Open i2c bus 1 and read one byte from address 7 offset 0
 bus = SMBus(bus=1)
